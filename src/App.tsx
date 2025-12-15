d72c0d754f083e0e1c914291dcf19fc2a835ef71
ERDINC DASKIN <eruanio33@gmail.com>
2025-12-15 15:33:14 +0100

    asdsad

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+
 import './App.css'
 import NasTym from './components/nasTym'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
